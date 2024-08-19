0ea162cd6e27fcfe6b44d085fb06d67d196c2883
QuanghuyDrawn <21130376@st.hcmuaf.edu.vn>
2024-08-19 22:55:04 +0700

    update

--- dotnet_backend/obj/Debug/net8.0/dotnet_backend.AssemblyInfo.cs
+++ dotnet_backend/obj/Debug/net8.0/dotnet_backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet_backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b7551cf25d33b11b8055a2179be729f1dd3229a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f796e178641c46b819b36351f62d83871d0f2366")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet_backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet_backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
